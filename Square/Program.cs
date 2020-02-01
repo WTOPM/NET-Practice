3de007d0c6079a0fc35f957a474515a44d1c5f41
WTOPM <wtopm@list.ru>
2020-02-01 20:12:22 +0200

    Square of the triangle

---
+++ Square/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.IO;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Heron's formula states that the area of triangle:");
+
+            Console.WriteLine($"Edit AB line:");
+            double ab = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Edit BC line:");
+            double bc = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Edit CB line:");
+            double cb = double.Parse(Console.ReadLine());
+
+            Console.Clear();
+            Console.WriteLine($"AB: {ab}, BC: {bc}, CB: {cb}.");
+
+            double p = (ab + bc + cb) / 2;
+            double sqrt = Math.Sqrt(p * (p - ab) * (p - bc) * (p - cb));
+
+            Console.WriteLine($"Square of the triangle: {Math.Round(sqrt, 2)}");
+        }
+            
+    }
+
+}
+
+
+
+
+
