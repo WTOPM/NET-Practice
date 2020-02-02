bfc17d88415e88d45ff4d56449dcbc1083372791
WTOPM <wtopm@list.ru>
2020-02-02 15:10:25 +0200

    Factorial

---
+++ Factorial/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Enter the factorial:");
+            int num = int.Parse(Console.ReadLine());
+            int i = 1;
+            double res = 0;
+            while (i <= num)
+            {
+                if (i == 1)
+                res = 1;
+                res *= i;
+                i++;
+            }
+            Console.WriteLine(res);
+        }
+    }
+}
+
+
+
+
+
+
+
