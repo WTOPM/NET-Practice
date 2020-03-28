5a9941c425dfc35525a275c16bb433cd48b3e66e
Maksym Makar <wtopm@list.ru>
2020-03-28 21:59:46 +0200

    Inheritance

---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class Person
+    {
+        string name;
+        int age;
+
+        public Person(string name)
+        {
+            this.name = name;
+            Console.WriteLine("Person(string name)");
+        }
+        public Person(string name, int age) : this(name)
+        {
+            this.age = age;
+            Console.WriteLine("Person(string name, int age)");
+        }
+    }
+    class Employee : Person
+    {
+        string company;
+
+        public Employee(string name, int age, string company) : base(name, age)
+        {
+            this.company = company;
+            Console.WriteLine("Employee(string name, int age, string company)");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee tom = new Employee("Tom", 22, "Microsoft");
+        }
+    }
+}
