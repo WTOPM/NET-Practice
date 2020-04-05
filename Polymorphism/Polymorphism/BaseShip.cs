41cbe138de1e7e222e7468c9d9d7624be76068e9
Maksym Makar <wtopm@list.ru>
2020-04-05 16:59:15 +0300

    Implement tested Inheritance

---
+++ Polymorphism/Polymorphism/BaseShip.cs
@@ -0,0 +1,26 @@
+﻿namespace Polymorphism
+{
+    public class BaseShip // :System.Object
+    {
+        
+        private int _counter;
+        protected int _speed;
+
+        public string Move(int distance)
+        {
+            _counter++;
+
+            string result = string.Format("Пройдено километров: {0}", distance);
+
+            return result;
+        }
+    }
+
+    public class TransportShip : BaseShip
+    {
+        public void Start()
+        {
+
+        }
+    }
+}
---
+++ Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TransportShip ship = new TransportShip();
+
+            string res = ship.Move(1000); // Inheritance
+
+            Console.WriteLine(res);
+            Console.ReadLine();
+        }
+    }
+}
