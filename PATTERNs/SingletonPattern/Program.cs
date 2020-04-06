ff05083776c5039d30e9cd64d27c6880688a25df
Maksym Makar <wtopm@list.ru>
2020-04-06 15:43:29 +0300

    Singleton pattern implementation

---
+++ PATTERNs/SingletonPattern/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton s1 = Singleton.Initialize();
+
+            Console.WriteLine(s1.GetHashCode());
+
+            Singleton s2 = Singleton.Initialize();// - содержит ссылку на s1
+
+            Console.WriteLine(s2.GetHashCode());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PATTERNs/SingletonPattern/Singleton.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonPattern
+{
+    class Singleton
+    {
+        private static Singleton single = null;// - Приватное статическое поле single типа Singleton с присвоенным значением null
+
+        protected Singleton()// - protected конструктор по умолчанию
+        {
+            
+        }
+
+        public static Singleton Initialize()// - Фабричный метод
+        {
+            if (single == null)// - Содержит ли переменная single в себе ссылку на обьект
+                single = new Singleton();// - присваиваем ссылку на новый экземпляр класса Singleton
+
+            return single;
+        }
+    }
+}
